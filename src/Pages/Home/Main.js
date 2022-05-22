8499d843ecdb6cdb991308eb61663540ab42e778
Rafin_31 <asifhossain976@gmail.com>
2022-05-22 22:32:58 +0600

    Sale Section  added

--- src/Pages/Home/Main.js
+++ src/Pages/Home/Main.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './Home.css'
 import Banner from './Banner';
 import Categories from './Categories';
+import Sale from './Sale';
 
 const Main = () => {
     return (
         <div className="max-w-7xl min-w-full max-auto">
             <Banner />
             <Categories />
+            <Sale />
         </div>
     );
 };
--- src/Pages/Home/Sale.js
+++ src/Pages/Home/Sale.js
@@ -2,8 +2,37 @@ import React from 'react';
 
 const Sale = () => {
     return (
-        <div>
-            test
+        <div className=' h-[30vh] flex justify-center items-center grid gap-10 mobile:px-10 laptop:px-20 desktop:px-20 mobile:grid-cols-1 desktop:grid-cols-3 laptop:grid-cols-3'>
+            <div className="saleBox saleBox-1 justify-center">
+                <div className="card rounded-none lg:max-w-lg ">
+                    <div className="card-body">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum vel delectus provident eligendi molestias id perspiciatis qui a similique accusantium.
+                    </div>
+                </div>
+            </div>
+            <div className="saleBox saleBox-2">
+                <div className="card rounded-none lg:max-w-lg ">
+                    <div className="card-body">
+
+                    </div>
+                </div>
+            </div>
+            <div className="saleBox saleBox-3 grid gap-5 grid-cols-1 grid-rows-2 ">
+                <div className="subBox subBox-1">
+                    <div className="card rounded-none lg:max-w-lg ">
+                        <div className="card-body">
+
+                        </div>
+                    </div>
+                </div>
+                <div className="subBox subBox-2">
+                    <div className="card rounded-none lg:max-w-lg ">
+                        <div className="card-body">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
